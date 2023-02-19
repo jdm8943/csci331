55dcb916eacda15f1ffdbb343fb28b882f05e57f
jdm8943 <jdm8943@rit.edu>
2023-02-19 18:31:47 -0500

    add city file

--- proj01/Search.java
+++ proj01/Search.java
@@ -1,6 +1,11 @@
 public class Search {
 
-        public static void main(){
+        private final String CITY_FILE = "city.dat";
+        private final String EDGE_FILE = "edge.dat";
+        private String input_file = "stdin";
+        private String out_file = "stdout";
+        
+        public static void main(String[] args) {
                 
         }
 }
