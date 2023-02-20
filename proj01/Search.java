bc35353f6903be5f776e73c40953feb39e70612e
jdm8943 <jdm8943@rit.edu>
2023-02-19 19:30:01 -0500

    file cant be found

--- proj01/Search.java
+++ proj01/Search.java
@@ -1,4 +1,6 @@
+import java.io.*;
 import java.util.*;
+
 
 public class Search {
 
@@ -8,29 +10,51 @@ public class Search {
         private static String out_file = "stdout";
         private static HashMap<City,ArrayList<City>> city_graph;
 
-        private static HashMap<String,City> parse_cities(String fn){
+        private static HashMap<String,City> parse_cities(String fn) throws FileNotFoundException{
                 HashMap<String,City> cities = new HashMap<>();
-                Scanner in = new Scanner(fn);
+                Scanner in = new Scanner(new File(fn));
                 while(in.hasNext()){
-                        City city = new City(in.next(),in.next(),Double.parseDouble(in.next()),Double.parseDouble(in.next()));
+                        City city = new City(
+                                in.next(),                      //name
+                                in.next(),                      //abbrev
+                                Double.parseDouble(in.next()),  //lat
+                                Double.parseDouble(in.next())   //lon
+                                );
                         cities.put(city.getName(), city);
                 }
                 in.close();
                 return cities;
         }
 
-        private static HashMap<City,ArrayList<City>> parse_graph(String fn, HashMap<String, City> cities){
+        private static HashMap<City,ArrayList<City>> parse_graph(String fn, HashMap<String, City> cities) throws FileNotFoundException{
                 HashMap<City,ArrayList<City>> graph = new HashMap<City,ArrayList<City>>();
-                Scanner in = new Scanner(fn);
+                Scanner in = new Scanner(new File(fn));
                 while(in.hasNext()){
-                        String 
+                        City city1 = cities.get(in.next());
+                        City city2 = cities.get(in.next());
+                        if (graph.containsKey(city1)){
+                                graph.get(city1).add(city2);
+                        } else {
+                                ArrayList<City> neighbors = new ArrayList<>();
+                                neighbors.add(city2);
+                                graph.put(city1,neighbors);
+                        }
                 }
                 return graph;
         }
 
         public static void main(String[] args) {
-                HashMap<String,City> cities = parse_cities(CITY_FILE);
-                city_graph = parse_graph(EDGE_FILE, cities);
+                HashMap<String, City> cities;
+                try {
+                        cities = parse_cities(CITY_FILE);
+                        System.out.println(cities.values());
+                        city_graph = parse_graph(EDGE_FILE, cities);
+                        System.out.println(city_graph);
 
+                } catch (FileNotFoundException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                }
+                
         }
 }
