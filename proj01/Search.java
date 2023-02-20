446f64fa548729bfbc6096593d1582b691c105b6
jdm8943 <jdm8943@rit.edu>
2023-02-19 19:08:17 -0500

    addds parse cities, starts parse graph

--- proj01/Search.java
+++ proj01/Search.java
@@ -1,11 +1,36 @@
+import java.util.*;
+
 public class Search {
 
-        private final String CITY_FILE = "city.dat";
-        private final String EDGE_FILE = "edge.dat";
-        private String input_file = "stdin";
-        private String out_file = "stdout";
-        
+        private final static String CITY_FILE = "city.dat";
+        private final static String EDGE_FILE = "edge.dat";
+        private static String input_file = "stdin";
+        private static String out_file = "stdout";
+        private static HashMap<City,ArrayList<City>> city_graph;
+
+        private static HashMap<String,City> parse_cities(String fn){
+                HashMap<String,City> cities = new HashMap<>();
+                Scanner in = new Scanner(fn);
+                while(in.hasNext()){
+                        City city = new City(in.next(),in.next(),Double.parseDouble(in.next()),Double.parseDouble(in.next()));
+                        cities.put(city.getName(), city);
+                }
+                in.close();
+                return cities;
+        }
+
+        private static HashMap<City,ArrayList<City>> parse_graph(String fn, HashMap<String, City> cities){
+                HashMap<City,ArrayList<City>> graph = new HashMap<City,ArrayList<City>>();
+                Scanner in = new Scanner(fn);
+                while(in.hasNext()){
+                        String 
+                }
+                return graph;
+        }
+
         public static void main(String[] args) {
-                
+                HashMap<String,City> cities = parse_cities(CITY_FILE);
+                city_graph = parse_graph(EDGE_FILE, cities);
+
         }
 }
